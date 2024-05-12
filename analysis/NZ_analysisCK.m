43eee830e854e5c030cf8b85c47e335d7367bb88
Chris Klink <p.c.klink@gmail.com>
2024-05-12 23:50:27 +0200

    NZ preprocess

--- analysis/NZ_analysisCK.m
+++ analysis/NZ_analysisCK.m
@@ -1,4 +1,4 @@
-%% Data processing plan 
+%% Data processing 
 DataFld = '/Users/chris/Documents/TEACHING/UU/PF/UCU_thesis/2024/BR_UCU/log';
 AnalysisFld = '/Users/chris/Documents/TEACHING/UU/PF/UCU_thesis/2024/BR_UCU/analysis';
 cd(DataFld);
@@ -7,6 +7,7 @@ folders = contents([contents.isdir]);
 
 %% Load the data
 for f = 1:length(folders)
+    fprintf(['Getting data from session ' num2str(f) '/' num2str(length(folders)) '\n'])
     warning off
     D(f).log = load(fullfile(folders(f).folder,folders(f).name,'logfile.mat'));
     eyefile = dir(fullfile(folders(f).folder,folders(f).name,'eyedata*'));
@@ -16,7 +17,22 @@ for f = 1:length(folders)
 end
 
 %% Process the data
+% Create a table to collect data
+T={};
+vNames = {'SesID','SesFld','Subject','Age','Gender','Handedness',...
+    'Block','Report','TrialNum','TrialType','PreStim','StimL', 'StimR',...
+    'Attention','PrestimResponse','PreStimEnd','PreStimTransient',...
+    'StimResponse','Eye_slope','Eye_pval',...
+    'Eye_meandiff','Eye_sumdiff','Eye_stddiff','mVel'};
+row=1;
 for d = 1:length(D)  % datasets
+    fprintf(['Data session ' num2str(d) '\n']);
+    % - Demographic info (age, sex, left/right handed, participant ID) 
+    SUBJECT = D(d).log.log.Subject;
+    AGE = D(d).log.log.Age;
+    GENDER = D(d).log.log.Gender;
+    HANDEDNESS = D(d).log.log.Handedness;
+
     traw = D(d).log.log.ev.t;
     tt = traw - traw(1);
 
@@ -52,11 +68,9 @@ for d = 1:length(D)  % datasets
     s_kri = find(strcmp(D(d).log.log.ev.type,'PostStimResponse').*strcmp(D(d).log.log.ev.info,'RightArrow'));
     s_keyright = tt(s_kri);
 
-
-
-    %%
     eyeX = D(d).eye.data.gaze.left.gazePoint.inUserCoords(1,:)./...
         D(d).log.settings.monitor.Deg2Pix;
+    eyeX(~D(d).eye.data.gaze.left.gazePoint.valid) = NaN;
     eyeT = 0:1/60:(length(eyeX)-1)/60;
     eyeTd = D(d).eye.data.gaze.systemTimeStamp;
     % rereference time
@@ -65,68 +79,144 @@ for d = 1:length(D)  % datasets
     bs1ei = find(eyeTd <= bs1e,1,"last");
     tt(end) = eyeT(bs1ei) - bs1;
     dt = eyeT(bs1ei) - bs1;
-    eyeT2 = eyeT -dt;
-
-
+    eyeT2 = eyeT - dt;
 
     firstblocktrial = 1;
     for b = 1:length(blockstarts) % blocks
         SessNr = d;
         SessFld = folders(d).name;
-        BlockNr = b;
-        BlockType = D(d).log.settings.block(b).reportmode;
-        nTrials = length(D(d).log.settings.block(b).trials);
-        trialsthisblock = firstblocktrial:firstblocktrial+nTrials;
-        firstblocktrial = trialsthisblock(end)+1;
-        ttb=0;
-        for ti = trialsthisblock
-            ttb = ttb+1;
-            TrialNr = ti;
-            TrialType = D(d).log.settings.block(b).trials(ttb);
-            
-
-        end
-
-        for t = 1:length(trialstarts) % trials
-            TrialNr = t;
-            TrialType = D(d).log.settings.block(b).trials(t);
-
-
-
+        BlockNr = D(d).log.settings.expt.blockorder(b);
+        fprintf(['Block ' num2str(BlockNr) '\n']);
+        BlockType = D(d).log.settings.block(BlockNr).reportmode;
+        nTrials = length(D(d).log.settings.block(BlockNr).trials);
+        trialsthisblock = firstblocktrial:firstblocktrial+nTrials-1;
         
+        ttb=0;
+        for ti = 1:nTrials
+            TrialNr = trialsthisblock(ti); 
+            ttb = ttb+1;
+            TrialType = D(d).log.settings.block(BlockNr).trials(ttb);
+
+            PreStim = D(d).log.settings.trialtype(TrialType).prestim;
+            StimEye1 = D(d).log.settings.trialtype(TrialType).eye(1).stim;
+            StimEye2 = D(d).log.settings.trialtype(TrialType).eye(2).stim;
+            Attention = D(d).log.settings.prestim(PreStim).attentiontype;
+
+            psi_now = psstarts(ti);
+            switch Attention
+                case 'exogenous'
+                    PrestimResponse = 'none';
+                    PreStimEnd = NaN;
+                    PreStimTransient = D(d).log.settings.prestim(PreStim).transient.stim;
+                case 'endogenous'
+                    l1 = find(ps_keyleft > psi_now, 1, 'first');
+                    r1 = find(ps_keyright > psi_now, 1, 'first');
+                    if l1 < r1
+                        PrestimResponse = 'left';
+                    else
+                        PrestimResponse = 'right';
+                    end
+                    PreStimEnd = D(d).log.settings.prestim(PreStim).driftspeed(2,1);
+                    PreStimTransient = NaN;
+            end
+            
+            si_now = trialstarts(ti);
+            switch BlockType
+                case 'key'
+                    l1 = find(s_keyleft > si_now, 1, 'first');
+                    r1 = find(s_keyright > si_now, 1, 'first');
+                    if l1 < r1
+                        StimResponse = 'left';
+                    else
+                        StimResponse = 'right';
+                    end
+                case 'none'
+                    StimResponse  = 'none';
+            end
+
+
+            % eye ----
+            % take signal from first second
+            duration = 1.5;
+            ei1 = find(eyeT2 >= si_now - 1, 1, 'first');
+            ei2 = find(eyeT2 <= si_now + duration, 1, 'last');
+            EYEX = eyeX(ei1:ei2);
+            % linear regression
+            timeeye = eyeT2(ei1:ei2);
+            incidx = ~isnan(EYEX) & (timeeye>=si_now) & (timeeye<=si_now+1);
+                        
+            warning off
+            lm = fitlm(timeeye(incidx),EYEX(incidx));
+            slope = lm.Coefficients.Estimate("x1");
+            pval = lm.Coefficients.pValue("x1");
+            warning on
+            % some simple values
+            dEYE_SUM = sum(diff(EYEX(incidx)));
+            dEYE_MEAN = mean(diff(EYEX(incidx)));
+            dEYE_STD = std(diff(EYEX(incidx)));
+            % --------
+              
+            % figure
+            if row ==1
+                f=figure('visible','off');
+            end
+
+            x = EYEX; t = timeeye;
+            v = (diff(x))*60; % velocity in deg/s
+            sw = round(60*0.5); % smoothing window 500 ms
+
+            subplot(2,1,1); hold off;
+            plot(t,x,'o-'); hold on;
+            plot([t(1) t(end)],[0 0],'w');
+            plot([si_now si_now],[-1,1]*max(abs(x)),'y');
+            plot([si_now+1 si_now+1],[-1,1]*max(abs(x)),'y--');
+            plot(timeeye(incidx),lm.Coefficients.Estimate("x1")*timeeye(incidx) + ...
+                lm.Coefficients.Estimate("(Intercept)"));
+            yr = max(abs(x(t>si_now & t<si_now+1)));
+            if ~isnan(yr) && yr
+                set(gca,'xlim',[si_now-0.5 si_now+1.5],'ylim',[-1,1]*yr);
+            else
+                set(gca,'xlim',[si_now-0.5 si_now+1.5]);
+            end
+            title('X position')
+
+            subplot(2,1,2); hold off;
+            yrr = smooth(v,sw);
+            yr = max(abs(yrr(t>si_now & t<si_now+1)));
+            plot(t(2:end),smooth(v,sw),'o-'); hold on;
+            plot([t(1) t(end)],[0 0],'w');
+            plot([si_now si_now],[-1,1]*max(abs(smooth(v,sw))),'y');
+            plot([si_now+1 si_now+1],[-1,1]*max(abs(smooth(v,sw))),'y--');
+            if ~isnan(yr) && yr
+                set(gca,'xlim',[si_now-0.5 si_now+1.5],'ylim',[-1,1]*yr);
+            else
+                set(gca,'xlim',[si_now-0.5 si_now+1.5]);
+            end
+            title('Horizontal velocity (smoothed')
+
+            sgtitle(['Eyetrace --- ' SUBJECT ' B-' num2str(BlockNr) ...
+                ' T-' num2str(TrialNr)]);
+
+            idx = (t>=si_now) & (t<=si_now+1);
+            mVel = mean(smooth(v(idx),sw));
+            
+            [~,~] = mkdir('NZ_eye');
+            fn = ['eye_csvrow_'  sprintf('%03d', row) '.png'];
+            set(gcf,"Position",[100 100 400 800], 'InvertHardcopy', 'off')
+            saveas(f,fullfile('NZ_eye',fn));
+
+            newRow = {SessNr,SessFld,SUBJECT,str2double(AGE),GENDER,HANDEDNESS,...
+                BlockNr,BlockType,TrialNr,TrialType,PreStim,StimEye1,StimEye2,...
+                Attention,PrestimResponse,PreStimEnd,PreStimTransient,...
+                StimResponse,slope,pval,dEYE_MEAN,dEYE_SUM,dEYE_STD,mVel};
+            T = [T; newRow];
+
+            row = row+1;
+        end
+        firstblocktrial = trialsthisblock(end)+1;
     end
-
-
-%% Extract the time of 1st event (beginning of 1st trial of 1st block) 
-% from the time of the other events to make comprehensive time units starting from 0 
-
-% Mark the start and end of each block and trial 
-
-% Mark if there was a left or right key-press at the end of every endo prestim phase 
-
-% In report condition mark if there was right or left key-press at the end of every trial 
-
-% Eye tracking data (only for no-report conditions, I%m not using the eye data for report condition) 
-
-% Get eye-trace information per trial (so mark events on eye-trace data as well, align with behavioral data) 
-
-% To determine left or right OKN: (I am not sure which one would work better – the first one is more simple but the second one might be more reliable?) 
-% Compute average eye position per trial and determine left or right based on pos or neg value 
-% Maybe compute average eye position just some time in the beginning of the trial (e.g. 1 s) 
-% so that initial dominance is measured but I couldn’t find information on how long is the OKN and how much time is needed to detect it 
-
-% For this: have to smooth the data, remove blinks so that average is not skewed? 
-% OR 
-% Compute velocity, then set a threshold for velocity of a saccade and detect saccade based 
-% on this threshold being crossed –> determine direction based on saccade direction (opposite direction of stimulus movement) 
-
 end
 
-% In the end create a table with: 
-% - Demographic info (age, sex, left/right handed, participant ID) 
-% - Endo/exo  
-% - Report/no-report  
-% - Catch/rivalry  
-% - OKN direction (left/right)  
-% - Button-press (left/right) 
-% - Correctness of endo key-press compared to instruction (correct/incorrect) 
+%% Save the results
+T = cell2table(T,"VariableNames",vNames);
+writetable(T,'NZ_RESULTS.csv')
